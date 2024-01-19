4d9d39c8f74ea1be12034bcd5af038a5f595e5e4
aaqibraza92 <aaqibraza92@gmail.com>
2024-01-19 18:59:19 +0530

    u

---
+++ src/pages/fine-jewelry/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Container } from 'react-bootstrap'
+
+const FineJewelry = () => {
+  return (
+  <section>
+    <Container>
+        <h3>
+            Hello
+        </h3>
+    </Container>
+  </section>
+  )
+}
+
+export default FineJewelry
