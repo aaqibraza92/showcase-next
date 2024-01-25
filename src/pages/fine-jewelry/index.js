ee92e03f88655f68d5ae8854813ad2360c3a868e
aaqibraza92 <aaqibraza92@gmail.com>
2024-01-25 16:36:05 +0530

    update

--- src/pages/fine-jewelry/index.js
+++ src/pages/fine-jewelry/index.js
@@ -30,6 +30,7 @@ const FineJewelry = ({ data }) => {
                 </select>
               </Col>
               <Col lg={6}>
+                View <svg class="MuiSvgIcon-root MuiSvgIcon-fontSizeMedium active_grid ml-2 css-vubbuv" focusable="false" aria-hidden="true" viewBox="0 0 24 24" data-testid="AppsIcon"><path d="M4 8h4V4H4zm6 12h4v-4h-4zm-6 0h4v-4H4zm0-6h4v-4H4zm6 0h4v-4h-4zm6-10v4h4V4zm-6 4h4V4h-4zm6 6h4v-4h-4zm0 6h4v-4h-4z"></path></svg>
               </Col>
             </Row>
           </Col>
