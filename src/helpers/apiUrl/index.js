40f1e8a498e97d64a9f58a9f01b42b6227889f25
aaqibraza92 <aaqibraza92@gmail.com>
2024-01-25 15:47:25 +0530

    update

--- src/helpers/apiUrl/index.js
+++ src/helpers/apiUrl/index.js
@@ -2,9 +2,10 @@ export const apiBaseUrl= "https://showcase.demobw.com/webapi/api/"
 export const megaMenu= apiBaseUrl+"home/megamenu?device_type=1"
 export const loginApi= apiBaseUrl+"/user/login"
 export const newsletter= apiBaseUrl+"common/subscribe_newsletter"
+export const productlist= apiBaseUrl+"product/productlist"
 
 export const ApiHeader={
-         // 'authorization':`Token e222085edc871220cae528152b73c1317ba2addf`,
+     // 'authorization':`Token e222085edc871220cae528152b73c1317ba2addf`,
     'Accept': 'application/json',
     'Content-Type': 'application/json'
 }
--- src/pages/fine-jewelry/index.js
+++ src/pages/fine-jewelry/index.js
@@ -1,16 +1,85 @@
+import { ApiHeader, productlist } from '@/helpers/apiUrl'
+import axios from 'axios'
 import React from 'react'
-import { Container } from 'react-bootstrap'
+import { Col, Container, Row } from 'react-bootstrap'
 
-const FineJewelry = () => {
+const FineJewelry = ({ data }) => {
+  console.log("data", data)
   return (
-  <section>
-    <Container>
-        <h3>
-            Hello
-        </h3>
-    </Container>
-  </section>
+    <section>
+      <Container>
+        <div className='wrapperData'>
+          <h3>
+            {data?.data?.meta_title}
+          </h3>
+          <p>
+            {data?.data?.meta_desp}
+          </p>
+        </div>
+
+        <Row>
+          <Col lg={3}>
+
+          </Col>
+          <Col lg={9}>
+            <Row>
+              <Col lg={6}>
+                <select name="" id="" className='form-control'>
+                <option value="">Sort by</option>
+                  <option value="featured">Featured</option>
+                </select>
+              </Col>
+              <Col lg={6}>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      </Container>
+    </section>
   )
 }
 
 export default FineJewelry
+
+export async function getServerSideProps() {
+  try {
+    const data = {
+      currency_code: "USD",
+      sort_by: "featured",
+      setwithmin: "0.51",
+      setwithmax: "0.60",
+      category: "fine-jewelry",
+      search_text: "",
+      filterdata: [],
+      limit: 18,
+      offset: 0,
+      user_id: "",
+      session_id: "",
+    };
+    return axios.post(productlist, data, {
+      headers: ApiHeader
+    }).then((response) => {
+      const data = response.data;
+      return {
+        props: {
+          data,
+        },
+      };
+    })
+      .catch((error) => {
+        console.error('Error fetching data:', error.message);
+        return {
+          props: {
+            data: {},
+          },
+        };
+      });
+  } catch (error) {
+    console.error('Error fetching data:', error.message);
+    return {
+      props: {
+        data: {},
+      },
+    };
+  }
+}
